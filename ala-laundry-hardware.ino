74bf02a497bbb55443c7fafb382fcc8ef26a93fe
DiasKabdualiyev <91250489+diaskabdualiev1@users.noreply.github.com>
2023-03-11 11:04:46 +0600

    Update ala-laundry-hardware.ino
    барын полностью переделал базовый стрим ксоытм жанныйларды оку ушын

--- ala-laundry-hardware.ino
+++ ala-laundry-hardware.ino
@@ -2,106 +2,86 @@
 #include <Firebase_ESP_Client.h>
 #include <addons/TokenHelper.h>
 #include <addons/RTDBHelper.h>
-#define WIFI_SSID "TechnoPark"
+#define WIFI_SSID "TechnoPark"  
 #define WIFI_PASSWORD "techno2020"
-#define API_KEY "AIzaSyDV1GlR0n3e6oVAPE7dauJy09LQj-vcoXY"
-#define DATABASE_URL "https://fir-esp32-project-default-rtdb.firebaseio.com"
+#define API_KEY "AIzaSyB95Rp0pvwjcFi0dHEvvrRh0svfTkuL7MA"
+#define DATABASE_URL "https://ala-laundry-902e5-default-rtdb.firebaseio.com"
+//#define USER_EMAIL "laundry01@gmail.com"
+//#define USER_PASSWORD "12345678"
 #define USER_EMAIL "USER_EMAIL"
 #define USER_PASSWORD "USER_PASSWORD"
 
-FirebaseData stream;
 FirebaseData fbdo;
 FirebaseAuth auth;
 FirebaseConfig config;
 
-FirebaseJsonData mode;
-FirebaseJsonData trigger;
-FirebaseJsonData door_status;
+unsigned long sendDataPrevMillis = 0;
 
-int default_mode = 7;
-int local_on_off = 0;
-int local_start_stop = 0;
-int local_door_status = 0;
-int local_trigger = 0;
+unsigned long count = 0;
 
-// pins
-int encoder_pin_1 = 26; // encoder pin 1
-int encoder_pin_2 = 25; // encoder pin 2
-int on_off_pin = 14;    // on/off button
-int start_stop_pin = 27; // start/stop button
-int door_pin = 13; // sensor pin to detect if the door is open or closed, uses pin 13 to avoid conflicts with other pins
-
-int on_off_status = 0;
-int start_stop_status = 0;
-int washing_mode = 7; // 12 modes in total, default is mode 6
-
-unsigned long send_data_prev_millis = 0;
-int count = 0;
-bool signup_ok = false;
-
-volatile bool data_changed = false;
 
 void streamCallback(FirebaseStream data)
 {
-  if (data.dataTypeEnum() == fb_esp_rtdb_data_type_json) {
-  FirebaseJson *json = data.to<FirebaseJson *>();
-  
-  // Get washing mode value from Firebase
-  if (json->get(mode, "mode")) {
-    washing_mode = mode.to<int>();
-    Serial.print("Washing mode: ");
-    Serial.println(washing_mode);
-  }
-
-  // Get wash trigger value from Firebase
-  if (json->get(trigger, "trigger")) {
-    local_trigger = trigger.to<int>();
-    Serial.print("Wash trigger: ");
-    Serial.println(local_trigger);
-  }
-  
-if (true) {
-  onOff(); // switch on the washing machine
-  int diffMode = default_mode - washing_mode; // calculate the difference between the default mode and the current washing mode
-  washing_mode = default_mode; // update the washing mode to the default mode
-  encoderScroll(diffMode); // scroll the encoder by the calculated difference
-  Serial.print("diffMode: ");
-  Serial.println(diffMode);
-  setDoor(1); // lock the door
-  startStop(); // start the washing machine
-  washing_mode = 7; // set the washing mode back to the default value
+  Serial.printf("stream path, %s\nevent path, %s\ndata type, %s\nevent type, %s\n\n",
+                data.streamPath().c_str(),
+                data.dataPath().c_str(),
+                data.dataType().c_str(),
+                data.eventType().c_str());
+  printResult(data); // see addons/RTDBHelper.h
+  Serial.println();
+  Serial.printf("Received stream payload size: %d (Max. %d)\n\n", data.payloadLength(), data.maxPayloadLength());
+  dataChanged = true;  
 }
-else {
-  Serial.println("else is running");
-  if (data.dataPath().equals("/mode")) { // check if the updated data is the washing mode
-    default_mode = data.to<int>(); // update the default mode with the received value
-    Serial.println(default_mode);
-  }
-  if (data.dataPath().equals("/trigger")) { // check if the updated data is the door trigger
-    local_trigger = data.to<int>(); // update the local trigger value with the received value
-    Serial.println(local_trigger);
-  }
-  if (local_trigger == 1) { // if the door is triggered
-     Serial.println("local_trigger = 1 ");
-     onOff(); // switch on the washing machine
-     int diffMode = default_mode - washing_mode; // calculate the difference between the default mode and the current washing mode
-     washing_mode = default_mode; // update the washing mode to the default mode
-     encoderScroll(diffMode); // scroll the encoder by the calculated difference
-     delay(2000);
-     Serial.print("diffMode: ");
-     Serial.println(diffMode);
-     setTrigger(1); // lock the door
-     startStop(); // start the washing machine
-     default_mode = 7; // set the default mode back to the default value
-     washing_mode = default_mode; // set the washing mode back to the default value
-     Serial.println(default_mode);
-  }
-  else { // if the door is not triggered
-    onOff(); // switch off the washing machine
-    washing_mode = 7; // set the washing mode back to the default value
-  }
- }
+void streamTimeoutCallback(bool timeout)
+{
+  if(timeout){
+    // Stream timeout occurred
+    Serial.println("Stream timeout, resume streaming...");
+  }  
 }
 
-data_changed = true; // indicate that data has been changed
+
+
+void setup(){
+  Serial.begin(115200);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting to Wi-Fi");
+  while (WiFi.status() != WL_CONNECTED){
+    Serial.print(".");
+    delay(300);
+  }
+  Serial.println();
+  Serial.print("Connected with IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.println();
+
+  Serial.printf("Firebase Client v%s\n\n", FIREBASE_CLIENT_VERSION);
+  config.api_key = API_KEY;
+  
+  /* Assign the RTDB URL (required) */
+  config.database_url = DATABASE_URL;
+
+if (Firebase.signUp(&config, &auth, "", "")){
+    Serial.println("ok");
+    signupOK = true;
+  }
+  else{
+    Serial.printf("%s\n", config.signer.signupError.message.c_str());
+  }
+
+  config.token_status_callback = tokenStatusCallback; // see addons/TokenHelper.h
+  Firebase.begin(&config, &auth);
+  Firebase.reconnectWiFi(true);
+
+  if (!Firebase.RTDB.beginStream(&stream, "/id1"))
+    Serial.printf("sream begin error, %s\n\n", stream.errorReason().c_str());
+  Firebase.RTDB.setStreamCallback(&stream, streamCallback, streamTimeoutCallback);
+}
+void loop()
+{
+ if (Firebase.ready())
+  {
+    // Firebase is ready to use now.
+    
+  } 
 }
